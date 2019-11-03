27959196e845dcc77316e87fd6b0a4f61a4a5de0
Michal Szafranski <szafranski@gmail.com>
2019-11-03 14:00:16 +0100

    Simplify enum name maps

--- src/consts.ts
+++ src/consts.ts
@@ -20,11 +20,7 @@ export enum HubType {
 
 
 // tslint:disable-next-line
-export let HubTypeNames = Object.keys(HubType).reduce((result: {[hubType: string]: string}, item) => {
-    // @ts-ignore
-    result[HubType[item]] = item;
-    return result;
-}, {});
+export const HubTypeNames = HubType;
 
 
 /**
@@ -71,11 +67,7 @@ export enum DeviceType {
 
 
 // tslint:disable-next-line
-export let DeviceTypeNames = Object.keys(DeviceType).reduce((result: {[deviceType: string]: string}, item) => {
-    // @ts-ignore
-    result[DeviceType[item]] = item;
-    return result;
-}, {});
+export const DeviceTypeNames = DeviceType;
 
 
 /**
@@ -110,11 +102,7 @@ export enum Color {
 
 
 // tslint:disable-next-line
-export let ColorNames = Object.keys(Color).reduce((result: {[color: string]: string}, item) => {
-    // @ts-ignore
-    result[Color[item]] = item;
-    return result;
-}, {});
+export const ColorNames = Color;
 
 
 /**
